258c2b46c91cdf77e326a6b2bb5c66c8cdf0b900
Alex Woolford <awoolford@deepthought.woolford.io>
2016-06-26 16:32:33 -0600

    Initial commit

---
+++ network-monitor-agent/src/main/java/io/woolford/NetworkMonitorAgentApplication.java
@@ -0,0 +1,12 @@
+package io.woolford;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkMonitorAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkMonitorAgentApplication.class, args);
+	}
+}
---
+++ network-monitor-agent/src/main/java/io/woolford/Ping.java
@@ -0,0 +1,7 @@
+package io.woolford;
+
+/**
+ * Created by awoolford on 6/26/16.
+ */
+public class Ping {
+}
